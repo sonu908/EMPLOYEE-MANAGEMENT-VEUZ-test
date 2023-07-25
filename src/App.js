47a11e1fc8efe238dd91579325a4db293c50ee35
Sonu S <sonusuresh908@gmail.com>
2023-07-25 23:24:12 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import EmpList from "./components/EmpList";
 function App() {
   return (
     <div className="App flex justify-center p-5">
-      <div className="w-3/4 bg-indigo-500 p-5 rounded-xl">
+      <div className="w-3/4 shadow-2xl border p-5 rounded-xl">
         {/* <FormDetails/> */}
         {/* <Routes>
         </Routes> */}
